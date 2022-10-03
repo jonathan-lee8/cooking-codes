4f6ec20569eadb76075d605e8e42dba9fbb80fdc
cwendland <chris.r.wendland@gmail.com>
2022-10-03 10:32:07 -0700

    Added routes for the recipe

--- controllers/api/recipeRoute.js
+++ controllers/api/recipeRoute.js
@@ -1,3 +1,21 @@
 const router = require('express').Router();
 
+//Get all recipes route
+router.get('/', (req, res) => {
+
+});
+
+//Get a single recipe route
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req,res) => {
+
+});
+
+router.delete('/:id', (req,res) => {
+
+});
+
 module.exports = router;
