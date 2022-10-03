fd709df3d0c09c4143e3fe57f2cd45a2a5349afa
cwendland <chris.r.wendland@gmail.com>
2022-10-03 10:58:47 -0700

    Added get route logic for recipe db

--- controllers/api/recipeRoute.js
+++ controllers/api/recipeRoute.js
@@ -1,13 +1,25 @@
 const router = require('express').Router();
+const Recipe = require('../../Models/Recipe');
 
 //Get all recipes route
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    const recipeData = await Recipe.findAll().catch((err) => { 
+      res.json(err);
+    });
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+    res.render('all', { recipes });
 });
 
 //Get a single recipe route
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+  try {
+  const recipeData = await Recipe.findByPk(req.params.id);
+  console.log(recipeData)
+  const recipe = recipeData.get({ plain: true });
+  res.render('recipe', recipe);
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 router.post('/', (req,res) => {
