227210703b1a2b1d70b091d40e0fc72381ff4dec
cwendland <chris.r.wendland@gmail.com>
2022-10-03 11:42:20 -0700

    Added post route for recipe and delete route for recipe

--- controllers/api/recipeRoute.js
+++ controllers/api/recipeRoute.js
@@ -10,14 +10,24 @@ router.post('/', async (req,res) => {
             total_time: req.body.total_time,
             image_url: req.body.image_url,
             course_name: req.body.course_name,
+            user_id: req.session.user_id, //Need to save user id in session and get it from there to add to recipe.
         });
+        res.status(200).json(dbRecipeData);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
 router.delete('/:id', (req,res) => {
-
+    Recipe.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+      .then((deletedRecipe) => {
+        res.json(deletedRecipe);
+      })
+      .catch((err) => res.json(err));
 });
 
 module.exports = router;
