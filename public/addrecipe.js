a3dfbc23908a9f216b29990fcbc2ed8ac567883c
Jonathan Lee <jonathanhlee8@gmail.com>
2022-10-05 15:11:57 -0700

    adding some js and more class ids

--- public/addrecipe.js
+++ public/addrecipe.js
@@ -1,5 +1,16 @@
 const newRecipe = async (event) => {
   event.preventDefault();
 
+  const appetizer = document.querySelector('.appetizer').value.trim();
+  const breakfast = document.querySelector('.breakfast').value.trim();
+  const lunch = document.querySelector('.lunch').value.trim();
+  const sides = document.querySelector('.sides').value.trim();
+  const dinner = document.querySelector('.dinner').value.trim();
+  const dessert = document.querySelector('.dessert').value.trim();
+
   
 }
+
+document
+  .querySelector('add-recipe-btn')
+  .addEventListener('submit', newRecipe);
